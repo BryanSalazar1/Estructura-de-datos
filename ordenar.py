0e7c46cd80efd2c1b7a56d92db400378a582f725
Bryan <bryansalazar184@gmail.com>
2024-01-30 09:07:29 -0600

    python

---
+++ ordenar.py
@@ -0,0 +1,15 @@
+mi_lista = [2,1,3,4,5,3,9]
+
+n = len(mi_lista)
+
+for i in range(n - 1):
+   
+    min_index = i
+   
+    for j in range(i + 1, n):
+        if mi_lista[j] < mi_lista[min_index]:
+            min_index = j
+    mi_lista[i], mi_lista[min_index] = mi_lista[min_index], mi_lista[i]
+
+
+print( mi_lista)
